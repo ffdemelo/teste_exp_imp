b1d6209140a28e6895e279ef7f22d9888fe6b6b6
FABRICIO FERNANDES DE MELO <ffdemelo2@gmail.com>
2023-09-17 13:31:28 -0300

    Update main.js usando prompt e alert

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // main.js
 import { somarNumeros } from './funcoes.js';
 
-const numero1 = 5;
-const numero2 = 10;
+/* const numero1 = 5;
+const numero2 = 10; */
+const numero1 = parseFloat(prompt("Digite o primeiro valor, viado!"));
+const numero2 = parseFloat(prompt("Agora o segundo, seu tapado!"));
 const resultado = somarNumeros(numero1, numero2);
 
-console.log(`A soma de ${numero1} e ${numero2} é igual a ${resultado}.`);
+alert(`Toma aí: ${numero1} e ${numero2} é igual a ${resultado}.`);
