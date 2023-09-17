0668dfb64100f6f3c45e29d63f6cacb439d33886
FABRICIO FERNANDES DE MELO <ffdemelo2@gmail.com>
2023-09-17 13:00:55 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,4 @@
+// funcoes.js
+export function somarNumeros(a, b) {
+    return a + b;
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+// main.js
+import { somarNumeros } from './funcoes.js';
+
+const numero1 = 5;
+const numero2 = 10;
+const resultado = somarNumeros(numero1, numero2);
+
+console.log(`A soma de ${numero1} e ${numero2} é igual a ${resultado}.`);
