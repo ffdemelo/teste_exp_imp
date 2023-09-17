b161611dca5b8dd8d16552a06007f1b843556d8f
FABRICIO FERNANDES DE MELO <ffdemelo2@gmail.com>
2023-09-17 14:05:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,4 +7,4 @@ const numero1 = parseFloat(prompt("Digite o primeiro valor!"));
 const numero2 = parseFloat(prompt("Agora o segundo, por favorzinho!"));
 const resultado = somarNumeros(numero1, numero2);
 
-alert(`TO resultado que não pode está muito preciso: ${numero1} e ${numero2} é igual a ${resultado}.`);
+alert(`O resultado que não pode está muito preciso: ${numero1} e ${numero2} é igual a ${resultado}.`);
