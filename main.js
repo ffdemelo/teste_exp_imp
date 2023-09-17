502cccefb41b68384c7dd6666126b73feba94b2b
FABRICIO FERNANDES DE MELO <ffdemelo2@gmail.com>
2023-09-17 14:04:43 -0300

    Update main.js para ser mais gentil

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import { somarNumeros } from './funcoes.js';
 
 /* const numero1 = 5;
 const numero2 = 10; */
-const numero1 = parseFloat(prompt("Digite o primeiro valor, viado!"));
-const numero2 = parseFloat(prompt("Agora o segundo, seu tapado!"));
+const numero1 = parseFloat(prompt("Digite o primeiro valor!"));
+const numero2 = parseFloat(prompt("Agora o segundo, por favorzinho!"));
 const resultado = somarNumeros(numero1, numero2);
 
-alert(`Toma aí: ${numero1} e ${numero2} é igual a ${resultado}.`);
+alert(`TO resultado que não pode está muito preciso: ${numero1} e ${numero2} é igual a ${resultado}.`);
